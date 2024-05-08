2a727c7381c054dc79d2ad4e94b5b9f78f57309d
Артём Киселёв <sartem2002@yandex.ru>
2024-05-08 19:14:05 +0300

    fix/pdf-parser

--- src/templates/teacher-interface-page/pdf-page/TestPdf.tsx
+++ src/templates/teacher-interface-page/pdf-page/TestPdf.tsx
@@ -1,28 +1,23 @@
 import axios from 'axios';
 import PdfReader from './PdfReader.js';
 import { useState } from 'react';
-// import useStore from '../../../store/store.jsx';
-
 
 export default function TestPdf() {
-    // const data = useStore.getState().jsonData;
-    const [fileName, setFileName] = useState(null);
+  const [fileName, setFileName] = useState<string | undefined>(undefined);
 
-    const TestPdf = async () => {
-        try {
-          const response = await axios.get("/api/generate-pdf", { responseType: 'blob' }); 
-          setFileName(response.data);
-          
-          console.log(response.data);
-        } catch (error) {
-          console.error('Ошибка при загрузке PDF файла:', error);
-        }
-      };
+  const TestPdf = async () => {
+    try {
+      const response = await axios.get("/api/generate-pdf", { responseType: 'blob' });
+      setFileName(response.data);
+    } catch (error) {
+      console.error('Ошибка при загрузке PDF файла:', error);
+    }
+  };
 
-    return (
-        <div>
-            <button onClick={() => TestPdf()}>Создать и отправить pdf</button>
-            {fileName && <PdfReader file={fileName} />}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => TestPdf()}>Создать и отправить pdf</button>
+      {fileName && <PdfReader file={fileName} />}
+    </div>
+  );
 }
